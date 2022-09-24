d7d63a3b456f63603708eb7e8f7b521a34acb0fd
Rodnei A. Ribeiro <rodnei.solucoes@gmail.com>
2022-09-24 11:09:29 -0300

    add search job by id

--- src/main/java/br/com/carraria/careersuggestions/controller/JobController.java
+++ src/main/java/br/com/carraria/careersuggestions/controller/JobController.java
@@ -5,14 +5,17 @@ import br.com.carraria.careersuggestions.model.Job;
 import br.com.carraria.careersuggestions.repository.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping(value = "/jobs", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/api/v1/jobs", produces = MediaType.APPLICATION_JSON_VALUE)
 public class JobController {
 
     @Autowired
@@ -22,4 +25,17 @@ public class JobController {
     public List<Job> list(){
         return repository.findAll();
     }
+
+    @GetMapping("/{jobId}")
+    public ResponseEntity<Job> search(@PathVariable Long jobId){
+        Optional<Job> jobs = repository.findById(jobId);
+
+        if(jobs.isPresent()){
+            return ResponseEntity.ok(jobs.get());
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
 }
